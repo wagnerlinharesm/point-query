d8f409825c9f57777c1a3ec8329b32c9228f512b
wagnerlm <wagnerlink@gmail.com>
2024-03-20 15:44:36 -0300

    fix

--- app/src/point_query_use_case.py
+++ app/src/point_query_use_case.py
@@ -1,4 +1,4 @@
-import psycopg2
+from ..psycopg2 import connect
 import os
 from fetch_user_name_password import fetch_username_password
 
@@ -12,7 +12,7 @@ def execute(matricula):
             'password': password,
             'host': os.getenv('DB_HOST')
         }
-        conn = psycopg2.connect(**database)
+        conn = connect(**database)
         cursor = conn.cursor()
 
         query = """SELECT f.matricula, f.email, p.data, pp.hora_entrada, pp.hora_saida, pp.horas_periodo, 
