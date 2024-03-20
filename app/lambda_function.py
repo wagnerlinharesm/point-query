fe679c15d5d6223b773d2a4f83a78df52ffa1c7c
wagnerlm <wagnerlink@gmail.com>
2024-03-20 15:48:54 -0300

    test

--- app/lambda_function.py
+++ app/lambda_function.py
@@ -5,22 +5,22 @@ from app.src.point_query_use_case import execute
 
 def handler(event, context):
     print(context)
-    try:
-        body = json.loads(event['body'])
-    except json.JSONDecodeError:
-        return {
-            'statusCode': 400,
-            'body': json.dumps({'error': 'Invalid JSON'})
-        }
-
-    if 'matricula' not in body:
-        return {
-            'statusCode': 400,
-            'body': json.dumps({'error': 'Missing matricula'})
-        }
-
-    matricula = body['matricula']
-    execute(matricula)
+    # try:
+    #     body = json.loads(event['body'])
+    # except json.JSONDecodeError:
+    #     return {
+    #         'statusCode': 400,
+    #         'body': json.dumps({'error': 'Invalid JSON'})
+    #     }
+    #
+    # if 'matricula' not in body:
+    #     return {
+    #         'statusCode': 400,
+    #         'body': json.dumps({'error': 'Missing matricula'})
+    #     }
+    #
+    # matricula = body['matricula']
+    # execute(matricula)
 
     return {
         'statusCode': 200,
