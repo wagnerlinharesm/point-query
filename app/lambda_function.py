7c41b271016589fe2ff32b110e5f0adea10f9d59
wagnerlm <wagnerlink@gmail.com>
2024-03-20 20:27:16 -0300

    improve code

--- app/lambda_function.py
+++ app/lambda_function.py
@@ -3,16 +3,9 @@ import base64
 from app.src.point_query_use_case import execute
 
 
-def handler(event, context):
-    print(context)
-    print(event)
+def handler(event):
     try:
-        token = event['headers'].get('Authorization', '').split(' ')[1]
-        payload = token.split('.')[1]
-        payload += '=' * ((4 - len(payload) % 4) % 4)
-        decoded_payload = base64.b64decode(payload).decode('utf-8')
-        token_payload = json.loads(decoded_payload)
-        username = token_payload.get('cognito:username')
+        username = extract_token(event)
         if not username:
             raise ValueError('Username not found in token')
     except Exception as e:
@@ -20,10 +13,19 @@ def handler(event, context):
             'statusCode': 400,
             'body': json.dumps({'error': str(e)})
         }
-    print(username)
     result = execute(username)
 
     return {
         'statusCode': 200,
         'body': json.dumps({'message': f'{result}'})
     }
+
+
+def extract_token(event):
+    token = event['headers'].get('Authorization', '').split(' ')[1]
+    payload = token.split('.')[1]
+    payload += '=' * ((4 - len(payload) % 4) % 4)
+    decoded_payload = base64.b64decode(payload).decode('utf-8')
+    token_payload = json.loads(decoded_payload)
+    username = token_payload.get('cognito:username')
+    return username
