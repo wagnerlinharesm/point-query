cea98674b5daebd0e748a65d513ac12adbce3a6e
wagnerlm <wagnerlink@gmail.com>
2024-03-20 20:23:00 -0300

    add decode

--- app/lambda_function.py
+++ app/lambda_function.py
@@ -1,4 +1,5 @@
 import json
+import base64
 from app.src.point_query_use_case import execute
 
 
@@ -6,21 +7,21 @@ def handler(event, context):
     print(context)
     print(event)
     try:
-        body = json.loads(event['body'])
-    except json.JSONDecodeError:
+        token = event['headers'].get('Authorization', '').split(' ')[1]
+        payload = token.split('.')[1]
+        payload += '=' * ((4 - len(payload) % 4) % 4)
+        decoded_payload = base64.b64decode(payload).decode('utf-8')
+        token_payload = json.loads(decoded_payload)
+        username = token_payload.get('cognito:username')
+        if not username:
+            raise ValueError('Username not found in token')
+    except Exception as e:
         return {
             'statusCode': 400,
-            'body': json.dumps({'error': 'Invalid JSON'})
+            'body': json.dumps({'error': str(e)})
         }
-
-    if 'matricula' not in body:
-        return {
-            'statusCode': 400,
-            'body': json.dumps({'error': 'Missing matricula'})
-        }
-
-    matricula = body['matricula']
-    result = execute(matricula)
+    print(username)
+    result = execute(username)
 
     return {
         'statusCode': 200,
