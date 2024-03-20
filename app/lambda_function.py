7b122d4adc5fea2f81beb620e0a384b64d723cd8
wagnerlm <wagnerlink@gmail.com>
2024-03-20 13:11:59 -0300

    add query

---
+++ app/lambda_function.py
@@ -0,0 +1,28 @@
+import json
+
+from src.point_query_use_case import execute
+
+
+def handler(event, context):
+    print(context)
+    try:
+        body = json.loads(event['body'])
+    except json.JSONDecodeError:
+        return {
+            'statusCode': 400,
+            'body': json.dumps({'error': 'Invalid JSON'})
+        }
+
+    if 'matricula' not in body:
+        return {
+            'statusCode': 400,
+            'body': json.dumps({'error': 'Missing matricula'})
+        }
+
+    matricula = body['matricula']
+    execute(matricula)
+
+    return {
+        'statusCode': 200,
+        'body': json.dumps({'message': f'Matrícula {matricula} processada com sucesso!'})
+    }
